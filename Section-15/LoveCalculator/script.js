5bb575a21ae7ff8e0d841e3df82e9f4281c574b7
V Yashwanth Raj <99544533+YashwanthRaj@users.noreply.github.com>
2024-09-06 11:46:13 -0400

    Add files via upload

---
+++ Section-15/LoveCalculator/script.js
@@ -0,0 +1,7 @@
+let name1 = prompt("Enter Your Name: ");
+let name2 = prompt("Enter Your crushes Name: ");
+
+let randomNumber = Math.random()
+randomNumber = Math.floor(randomNumber * 100) + 1;
+
+alert("Your love percentage is " + randomNumber + "%");
