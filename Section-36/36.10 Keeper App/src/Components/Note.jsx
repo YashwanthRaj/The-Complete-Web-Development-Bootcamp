cb030fc36d0ca684f3dac531c5aafa8886cf3fa3
V Yashwanth Raj <99544533+YashwanthRaj@users.noreply.github.com>
2025-02-10 12:50:54 -0500

    Add files via upload

---
+++ Section-36/36.10 Keeper App/src/Components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Title of Note Component</h1>
+      <p>Paragraph of Note Component</p>
+    </div>
+  );
+}
+
+export default Note;
